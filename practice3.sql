f636a3f9cdb1f824f7d1a7f58a855f56f25b01b5
ooxiaoyan <810978527@qq.com>
2018-05-07 15:22:51 +0800

    docs:add practice4.sql

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,10 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+SELECT companyName,AVG(salary) as avgSalary
+FROM company,employee
+WHERE company.id = employee.companyId
+GROUP BY companyName
+ORDER BY avgSalary DESC
+LIMIT 1
 
