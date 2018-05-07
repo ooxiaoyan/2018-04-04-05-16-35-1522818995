9f4d3b73a3d15ba5c08c8e39c5ef6a924b5bf4f8
ooxiaoyan <810978527@qq.com>
2018-05-07 15:21:36 +0800

    docs:add practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,10 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+
+SELECT companyName,name
+FROM
+(SELECT company.id,companyName,MAX(salary) as maxsal
+FROM employee,company
+where employee.companyId = company.id
+GROUP BY company.id,companyName) as a,employee as e
+where a.id = e.companyId AND maxsal = e.salary
